74bf0a305414a8c9e9555a2fcc04cab00c532c86
Hanamant-Daboji <sagar.tps05@gmail.com>
2025-03-21 02:29:16 +0530

    User Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,32 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
-
-
+import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+//import cartRouter from "./routes/cartRoute.js"
+//import orderRouter from "./routes/orderRoute.js"
 
 
 // app config
 const app = express()
-const port = 5173
+const port = 4000
 
 // middleware
 app.use(express.json())
 app.use(cors())
 
-//// db connection
+// db connection
 connectDB();
+
+// api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+//app.use("/api/cart",cartRouter)
+//app.use("/api/order",orderRouter)
+
+
 
 app.get("/",(req,res)=>{
     res.send("API Working")
@@ -23,3 +35,5 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
     console.log(`Server Started on http://localhost:${port}`)
 })
+
+// YOU CAN SAVE UR DATABASE IN THIS COMMENT IF U WANT --> 
